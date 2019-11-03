1bf6095630768b30bf237af4b61e132616fbe1d6
Userwei <973555246@qq.com>
2019-11-03 21:03:00 +0800

    方法练习2求1到100之间所有数字的和

--- src/com/company/demo01/DemoExercise02MethodSum.java
+++ src/com/company/demo01/DemoExercise02MethodSum.java
@@ -19,7 +19,7 @@ public class DemoExercise02MethodSum {
     public static int getSum() {
         int sum = 0;
         for (int i = 0; i < 100; i++) {
-            sum += 1;
+            sum += i;
         }
         return sum;
     }
