0897e60a3d2189eecfdd939fa8b71570b676f0f3
Userwei <973555246@qq.com>
2019-11-04 00:43:25 +0800

    方法的重载练习：实现重载的print

--- src/com/company/demo01/DemoExercise04MethodOverloadPrint.java
+++ src/com/company/demo01/DemoExercise04MethodOverloadPrint.java
@@ -11,35 +11,35 @@ public class DemoExercise04MethodOverloadPrint {
         myPrint("Hello");//String
     }
 
-    public static void myPrint(byte num){
+    public static void myPrint(byte num) {
         System.out.println(num);
     }
 
-    public static void myPrint(short num){
+    public static void myPrint(short num) {
         System.out.println(num);
     }
 
-    public static void myPrint(int num){
+    public static void myPrint(int num) {
         System.out.println(num);
     }
 
-    public static void myPrint(long num){
+    public static void myPrint(long num) {
         System.out.println(num);
     }
 
-    public static void myPrint(float num){
+    public static void myPrint(float num) {
         System.out.println(num);
     }
 
-    public static void myPrint(char zifu){
+    public static void myPrint(char zifu) {
         System.out.println(zifu);
     }
 
-    public static void myPrint(boolean is){
+    public static void myPrint(boolean is) {
         System.out.println(is);
     }
 
-    public static void myPrint(String str){
+    public static void myPrint(String str) {
         System.out.println(str);
     }
 
