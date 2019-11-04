7495173bbab2b7794e20657c45b1aa7f0967d19f
Userwei <973555246@qq.com>
2019-11-04 23:40:49 +0800

    数组

---
+++ src/com/company/demo01/Array/Demo06ArrayTow.java
@@ -0,0 +1,40 @@
+package com.company.demo01.Array;
+
+public class Demo06ArrayTow {
+    public static void main(String[] args) {
+        int[] arrayA =new int[3];
+        System.out.println(arrayA);//地址值
+        System.out.println(arrayA[0]);//0
+        System.out.println(arrayA[1]);//0
+        System.out.println(arrayA[2]);//0
+        System.out.println("==================");
+        
+        //改变数组当中元素的内容
+        arrayA[1] = 10;
+        arrayA[2] = 20;
+        System.out.println(arrayA);//地址值
+        System.out.println(arrayA[0]);//0
+        System.out.println(arrayA[1]);//10
+        System.out.println(arrayA[2]);//20
+        System.out.println("==================");
+
+
+        int[] arrayB =new int[3];
+        System.out.println(arrayB);//地址值
+        System.out.println(arrayB[0]);//0
+        System.out.println(arrayB[1]);//0
+        System.out.println(arrayB[2]);//0
+        System.out.println("==================");
+
+
+        arrayB[1] = 100;
+        arrayB[2] = 200;
+        System.out.println(arrayB);//地址值
+        System.out.println(arrayB[0]);//0
+        System.out.println(arrayB[1]);//100
+        System.out.println(arrayB[2]);//200
+        System.out.println("==================");
+
+
+    }
+}
