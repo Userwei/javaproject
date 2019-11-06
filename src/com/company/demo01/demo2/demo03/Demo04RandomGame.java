1ec1d83101cb3754e13d7e15d522120fd08448b8
Userwei <973555246@qq.com>
2019-11-06 21:33:35 +0800

    Random练习二：猜数字小游戏

---
+++ src/com/company/demo01/demo2/demo03/Demo04RandomGame.java
@@ -0,0 +1,69 @@
+package com.company.demo01.demo2.demo03;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+题目:
+用代码模拟猜数字的小游戏。
+
+思路：
+1.首先需要产生一个随机数字，并且一旦产生不再变化。用Random的nextInt方法
+2.需要键盘输入，所以用到了Scanner
+3.获取键盘输入的数字，用Scanner当中的nextInt方法
+4.已经得到了两个数字，判断（if）一下
+    如果太大了，提示太大，并且重试；
+    如果太小了，提示太小，并且重试；
+    如果猜中了，游戏结束。
+5.重试就是再来一次，循环次数不确定，用while(true)
+
+变形:题目要求重试10次
+*/
+public class Demo04RandomGame {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        int randomNum = r.nextInt(100) + 1; // [1-100]
+        Scanner sc = new Scanner(System.in);
+
+        /*while(true){
+            System.out.println("请输入你猜测的数字：");
+            int guessNum = sc.nextInt();//键盘输入猜测的数字
+
+            if(guessNum > randomNum) {
+                System.out.println("太大了，请重试。");
+            }else if(guessNum < randomNum){
+                System.out.println("太小了，请重试。");
+            }else{
+                System.out.println("恭喜你，猜中啦！");
+                break;//如果猜中，不再重试
+            }
+        }*/
+        int n = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("请输入你猜测的数字：");
+            int guessNum = sc.nextInt();//键盘输入猜测的数字
+
+            if (guessNum > randomNum) {
+                System.out.println("太大了，请重试。");
+            } else if (guessNum < randomNum) {
+                System.out.println("太小了，请重试。");
+            } else {
+                System.out.println("恭喜你，猜中啦！");
+                break;//如果猜中，不再重试
+            }
+            n++;
+            if (n == 10) {
+                System.out.println("你已经没有机会了");
+            } else {
+                System.out.println("你还有" + (10 - n) + "次机会");
+            }
+        }
+        if (n == 10) {
+            System.out.println("你好傻呀！这都猜不中");
+        }
+            System.out.println("游戏结束。");
+
+
+    }
+}
