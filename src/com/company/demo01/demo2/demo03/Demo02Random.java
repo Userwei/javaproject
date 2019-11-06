3c60e36d6b8d59b3c51b0255047a6aecaf5819b3
Userwei <973555246@qq.com>
2019-11-06 19:55:10 +0800

    Random概述和基本使用及生成指定范围的随机数

---
+++ src/com/company/demo01/demo2/demo03/Demo02Random.java
@@ -0,0 +1,14 @@
+package com.company.demo01.demo2.demo03;
+
+import java.util.Random;
+
+public class Demo02Random {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            int num = r.nextInt(10);//范围实际上是0-10
+            System.out.println(num);
+        }
+    }
+}
