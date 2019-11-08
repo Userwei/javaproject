5d62c50632f407c125ec16b3c2d6da66a5f5cb52
Userwei <973555246@qq.com>
2019-11-08 20:19:29 +0800

    ArrayList练习二_存储自定义

---
+++ src/com/company/demo01/demo2/demo05/Student.java
@@ -0,0 +1,31 @@
+package com.company.demo01.demo2.demo05;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+}
