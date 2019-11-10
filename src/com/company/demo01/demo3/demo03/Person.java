e3af3235bb34b009d146506b3df311228f9d7e83
Userwei <973555246@qq.com>
2019-11-10 11:24:23 +0800

    静态代码块

---
+++ src/com/company/demo01/demo3/demo03/Person.java
@@ -0,0 +1,13 @@
+package com.company.demo01.demo3.demo03;
+
+
+public class Person {
+
+    static {
+        System.out.println("静态代码块执行!");
+    }
+
+    public Person() {
+        System.out.println("构造方法执行！");
+    }
+}
