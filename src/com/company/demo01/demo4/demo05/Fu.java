3f22b449aa020e94eaed4f39b926e14fa049f72d
Userwei <973555246@qq.com>
2019-11-10 15:12:36 +0800

    继承中方法的覆盖重写概念及注意事项

---
+++ src/com/company/demo01/demo4/demo05/Fu.java
@@ -0,0 +1,8 @@
+package com.company.demo01.demo4.demo05;
+
+public class Fu {
+
+    public Object method(){
+        return null;
+    }
+}
---
+++ src/com/company/demo01/demo4/demo05/Zi.java
@@ -0,0 +1,9 @@
+package com.company.demo01.demo4.demo05;
+
+public class Zi extends Fu{
+
+    @Override
+    public String method(){
+        return null;
+    }
+}
