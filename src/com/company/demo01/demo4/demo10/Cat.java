2bc24ddd34d0e7d7262914c9c26de2a9d32e29b9
Userwei <973555246@qq.com>
2019-11-11 11:54:27 +0800

    抽象类和抽象方法的使用及注意事项

---
+++ src/com/company/demo01/demo4/demo10/Cat.java
@@ -0,0 +1,9 @@
+package com.company.demo01.demo4.demo10;
+
+public class Cat extends Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+}
---
+++ src/com/company/demo01/demo4/demo10/DemoMain.java
@@ -0,0 +1,10 @@
+package com.company.demo01.demo4.demo10;
+
+public class DemoMain {
+
+    public static void main(String[] args) {
+//        Animal animal = new Animal();// 错误写法！不能直接创建抽象类对象
+        Cat cat = new Cat();
+        cat.eat();
+    }
+}
