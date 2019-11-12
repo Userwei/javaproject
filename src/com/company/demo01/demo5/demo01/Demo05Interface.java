1279d6c3598ab0ca2fa6cd2aebb575873bad70fb
Userwei <973555246@qq.com>
2019-11-12 23:20:27 +0800

    接口的常量定义及使用

---
+++ src/com/company/demo01/demo5/demo01/Demo05Interface.java
@@ -0,0 +1,9 @@
+package com.company.demo01.demo5.demo01;
+
+public class Demo05Interface {
+
+    public static void main(String[] args) {
+        //  访问接口当中的常量
+        System.out.println(MyInterfaceConst.NUM);
+    }
+}
