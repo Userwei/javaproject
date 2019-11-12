f70fc214ca466f8ccf7ae1e1da0dc3c257818bef
Userwei <973555246@qq.com>
2019-11-12 22:52:38 +0800

    接口的静态方法定义及使用

---
+++ src/com/company/demo01/demo5/demo01/MyInterfaceStatic.java
@@ -0,0 +1,16 @@
+package com.company.demo01.demo5.demo01;
+
+/*
+从java 8开始，接口当中允许定义静态方法。
+格式：
+public static 返回值类型 方法名称（参数列表）{
+    方法体
+}
+提示：就是将abstract或者default换成static即可，带上方法体。
+*/
+public interface MyInterfaceStatic {
+
+    static void methodStatic(){
+        System.out.println("这是接口的静态方法！" );
+    }
+}
---
+++ src/com/company/demo01/demo5/demo01/MyInterfaceStaticImpl.java
@@ -0,0 +1,4 @@
+package com.company.demo01.demo5.demo01;
+
+public class MyInterfaceStaticImpl implements MyInterfaceStatic {
+}
