504e91f415cb6c9303a85423d4c8fb9fb2feec92
Userwei <973555246@qq.com>
2019-11-13 12:43:20 +0800

    多态的格式及使用

---
+++ src/com/company/demo01/demo5/demo03/Fu.java
@@ -0,0 +1,12 @@
+package com.company.demo01.demo5.demo03;
+
+public class Fu {
+
+    public void method(){
+        System.out.println("父类方法");
+    }
+
+    public void methodFu(){
+        System.out.println("父类特有方法");
+    }
+}
---
+++ src/com/company/demo01/demo5/demo03/Zi.java
@@ -0,0 +1,9 @@
+package com.company.demo01.demo5.demo03;
+
+public class Zi extends Fu{
+
+    @Override
+    public void method() {
+        System.out.println("子类方法");
+    }
+}
