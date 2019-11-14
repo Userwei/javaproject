7a01ddf912bd71cb3a2aab90713c07188b722d74
Userwei <973555246@qq.com>
2019-11-14 21:09:12 +0800

    局部内部类定义

---
+++ src/com/company/demo01/demo6/demo04/DemoMain.java
@@ -0,0 +1,11 @@
+package com.company.demo01.demo6.demo04;
+
+public class DemoMain {
+
+    public static void main(String[] args) {
+        Outer obj = new Outer();
+        obj.methodOuter();
+    }
+
+}
+
